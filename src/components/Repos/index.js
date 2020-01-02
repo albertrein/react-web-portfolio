2b593b4382fddc6be0b7eb0eb11764c2c2118a9d
albertrein <guilhermealberto8@gmail.com>
2020-01-02 10:01:20 -0300

    Inserinting link to github repository.

--- src/components/Repos/index.js
+++ src/components/Repos/index.js
@@ -8,23 +8,20 @@ export default class Main extends Component{
 	};
 
 	async componentDidMount(){		
-		// let response = await GithubAPI.headerDrops();
-		// this.setState({albertrein: response});
-		// console.log(this.state);
-		
 		let response = await GithubAPI.repoDrops();
 		this.setState({albertrein: response});
-		console.log(response);
 	}
 
 	render(){
 		return (
 			<div class="repos-content">
 				{this.state.albertrein.map(repo => (
-					<div class={'repository ' + repo.language} key={repo.id}>
-						<p class="title" >{repo.name}</p>
-						<p class="description">{repo.description}</p>
-					</div>
+					<a key={repo.id} href={repo.svn_url} target="_blank" rel = "noopener noreferrer">
+						<div class={'repository ' + repo.language} >
+							<p class="title" >{repo.name}</p>
+							<p class="description">{repo.description}</p>
+						</div>
+					</a>
 				))}
 			</div>
 		);
